e8ececa174a248084c4f005a483739cd2b7eef45
BaoHuiling <baozii.721@gmail.com>
2021-01-09 15:20:37 +0100

    fill in fonctions

--- quick_tools.py
+++ quick_tools.py
@@ -14,21 +14,29 @@ def get_rooms(db_path):
 
 def verify_room_name(room_name):
 	# Extra requirement: room_name start with ROOM_ and length >=8
-	
+	if room_name.startswith('ROOM_'):
+		if len(room_name) >= 8 :
+			return True
+	return False
 
 def verify_room_type(room_type):
 	# Extra requirement: room_type has to be public or private
-	
+	if room_type == 'public' or room_type == 'private':
+		return True
+	return False
 
 def add_room(db_path, room_name, room_type):
 	connect = sqlite3.connect(db_path)
 	cursor = connect.cursor()
 
+	if verify_room_type(room_type) and verify_room_name(room_name):
 
-	sql = 'INSERT INTO Rooms (room_name,room_type) VALUES (?,?)'
-	cursor.execute(sql,(room_name, room_type))
+		sql = 'INSERT INTO Rooms (room_name,room_type) VALUES (?,?)'
+		cursor.execute(sql,(room_name, room_type))
 
-	
+	else:
+		print("\nRooms ERROR: \n room_type has to be 'public' or 'private'\nroom_name has to start with ROOM_ and length>=8\n")
+
 	connect.commit()
 
 
@@ -56,7 +64,21 @@ def get_users(db_path):
 
 def verify_user_password(user_password):
 	# Extra requirement: check the password have number,special character, length>8 
-	
+	is_number = 0
+	special_character = 0
+
+	for i in user_password:
+		if i.isdigit():
+			is_number = 1
+
+		if (not i.islower()) and (not i.isupper())  and (not i.isdigit()):
+			special_character = 1
+
+	if len(user_password)>=8:
+		if is_number and special_character:
+			return True
+		
+	return False
 
 	
 def add_user(db_path, user_name, user_role, user_rights, user_password):
@@ -65,10 +87,13 @@ def add_user(db_path, user_name, user_role, user_rights, user_password):
 	cursor = connect.cursor()
 	
 	
-	sql = 'INSERT INTO Users (user_name, user_role, user_rights, user_password) VALUES (?,?,?,?)'
-	cursor.execute(sql,(user_name, user_role, user_rights, user_password))
+	if verify_user_password(user_password):
+		sql = 'INSERT INTO Users (user_name, user_role, user_rights, user_password) VALUES (?,?,?,?)'
+		cursor.execute(sql,(user_name, user_role, user_rights, user_password))
 
-
+	else:
+		print("\nUsers ERROR: password should > 8 chars, includes numbers and special character")
+	
 	connect.commit()
 
 
