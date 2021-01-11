5af83796d118833696799c01c3e83cc647b5c266
diegopanepo <diegomv1997@hotmail.com>
2021-01-12 00:00:10 +0100

    Mise a jour de test pour fonction verify_user_name

--- README.md
+++ README.md
@@ -11,4 +11,8 @@ En quoi le TDD et l'approche devops peuvent ils améliorer la qualité d'un logi
 
 user_name doit être unique et il ne doit contenir ni chiffre ni symbole.
 
-On va ajouter le test associé
+Step 1 :
+On a créé la fonction verify_user_name(user_name)
+
+Step 2 :
+On a créé la vérification pour la fonction verify_user_name(user_name)
--- test_quick_tools.py
+++ test_quick_tools.py
@@ -6,4 +6,30 @@ import string
 class QuickToolsTester(unittest.TestCase):
 
 	def test_verify_user_name(self):
-        pass
+
+		db_path = 'quick_chat.db'
+
+		str_lett = string.ascii_letters
+		test_str = ''.join(random.choice(str_lett) for _ in range(random.randint(2,6)))
+		self.assertTrue(verify_user_name(test_str))
+
+		str_chif = string.digits
+		test_str = ''.join(random.choice(str_chif) for _ in range(random.randint(2,6)))
+		self.assertFalse(verify_user_name(test_str))
+
+		str_symb = string.punctuation
+		test_str = ''.join(random.choice(str_symb) for _ in range(random.randint(2,6)))
+		self.assertFalse(verify_user_name(test_str))
+
+		try:
+			connect = sqlite3.connect(db_path)
+			cursor = connect.cursor()
+			sql = 'INSERT INTO Users (user_name) VALUES ("username")'
+			cursor.execute(sql)
+			cursor.execute(sql)
+		except IntegrityError: # exception sql
+			self.assertTrue(verify_user_name('username'))
+		else:
+			self.assertFalse(verify_user_name('username'))
+		finally:
+			cursor.close() # close sans commit pour ne pas enregistrer 'username'
