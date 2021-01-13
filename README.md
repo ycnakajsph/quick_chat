ca2cd8ad13efd9cfcf6cb2f8a6426f32b50ea068
ludollaoo <ludoduplessis@hotmail.fr>
2021-01-13 21:30:32 +0100

    etape2: test ko

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ### Etape 1: test ok
 
 Ajout d'un test avec un username correct
+
+### Etape 2: test ko
+
+Ajout de test avec des username incorrects :
+	- nombre
+	- caractere special
+	- doublon
--- TestQuickTools.py
+++ TestQuickTools.py
@@ -19,6 +19,32 @@ class TestQuickTools(unittest.TestCase):
 		sql = 'SELECT user_name FROM Users;'
 		users = cursor.execute(sql).fetchall()
 		self.assertTrue(('Ludollaoo',) in users)
+	
+	#ajout d'un nom d'utilisateur incorrect : nombre
+	def test_add_user_ko_nombre(self):
+		connect = sqlite3.connect(db_path)
+		cursor = connect.cursor()
+		
+		#'Ludollaoo1' est un username incorrect
+		quick_tools.add_user(db_path, 'Ludollaoo1', 0, 0, 'password')
+		
+		sql = 'SELECT user_name FROM Users;'
+		users = cursor.execute(sql).fetchall()
+		self.assertFalse(('Ludollaoo1',) in users)
+		
+	#ajout d'un nom d'utilisateur incorrect : caractère spécial
+	def test_add_user_ko_caractere_special(self):
+		connect = sqlite3.connect(db_path)
+		cursor = connect.cursor()
+		
+		#'Ludollaoo!' est un username incorrect
+		quick_tools.add_user(db_path, 'Ludollaoo!', 0, 0, 'password')
+		
+		sql = 'SELECT user_name FROM Users;'
+		users = cursor.execute(sql).fetchall()
+		self.assertFalse(('Ludollaoo!',) in users)
+	
+	
 		
 
 
